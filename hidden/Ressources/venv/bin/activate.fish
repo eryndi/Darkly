00bd1af591e9736fd1f4d3236c5311422b319b77
Diana WALD <dwald@e2r3p4.42.fr>
2019-10-03 17:32:19 +0200

    hidden venv

--- hidden/Ressources/venv/bin/activate.fish
+++ hidden/Ressources/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/dwald/Darkly/hidden/venv"
+set -gx VIRTUAL_ENV "/Users/dwald/Darkly/hidden/Ressources/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
